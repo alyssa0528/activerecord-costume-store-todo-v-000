a4e25c82636480f3346d3f80acbb5391dd0dc490
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-19 13:01:49 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,10 +1,11 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    t.string :name 
-    t.string :location 
-    t.integer :costumer_inventory 
-    t.integer :employees
-    t.boolean :
+    t.string :name
+    t.string :location
+    t.integer :costumer_inventory
+    t.integer :num_of_employees
+    t.boolean :still_in_business 
+    
   end
 end
