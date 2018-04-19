0e33d6fae9b79c0670e3bf0b1928beed21b317dc
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-19 12:37:30 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps :created_at
     end
   end
 end
