e75c17d000ac8bf451b1b112e86e86445e790bc8
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-19 13:08:59 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.timedate :opening_date
+      t.timedate :closing_date
+      t.string :description 
     end
   end
 end
